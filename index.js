2f78738243b292e7f4d9a65680d56f8657377671
Tobias Lidskog <tobias@lidskog.se>
2015-10-12 13:43:08 +0200

    Adding api to get module version.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 
-var path = require('path');
+var path = require('path'),
+  version = require('./package').version;
+
+module.exports.version = version;
 
 module.exports.binPath = function() {
   return path.resolve(__dirname, 'vendor', 'IEDriverServer.exe');
