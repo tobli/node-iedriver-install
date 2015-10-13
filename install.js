480ce078efe266fdb5c1c8cd1bda9e93adbbe97a
Tobias Lidskog <tobias@lidskog.se>
2015-10-13 15:19:25 +0200

    Update to IEDriverServer 2.48.0

--- install.js
+++ install.js
@@ -5,12 +5,12 @@ var Download = require('download'),
     os = require('os');
 
 function getIEDriverUrl() {
-  var urlBase = 'http://selenium-release.storage.googleapis.com/2.47/';
+  var urlBase = 'http://selenium-release.storage.googleapis.com/2.48/';
 
   if (os.arch() === 'x64') {
-    return urlBase + 'IEDriverServer_x64_2.47.0.zip';
+    return urlBase + 'IEDriverServer_x64_2.48.0.zip';
   } else {
-    return urlBase + 'IEDriverServer_Win32_2.47.0.zip';
+    return urlBase + 'IEDriverServer_Win32_2.48.0.zip';
   }
 }
 
