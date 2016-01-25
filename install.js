de3aa75ec2acd780e17bf5f1060ca77d57e47734
Tobias Lidskog <tobias@lidskog.se>
2016-01-25 08:32:40 +0100

    Update to IEDriver 2.49.

--- install.js
+++ install.js
@@ -5,12 +5,12 @@ var Download = require('download'),
     os = require('os');
 
 function getIEDriverUrl() {
-  var urlBase = 'http://selenium-release.storage.googleapis.com/2.48/';
+  var urlBase = 'http://selenium-release.storage.googleapis.com/2.49/';
 
   if (os.arch() === 'x64') {
-    return urlBase + 'IEDriverServer_x64_2.48.0.zip';
+    return urlBase + 'IEDriverServer_x64_2.49.0.zip';
   } else {
-    return urlBase + 'IEDriverServer_Win32_2.48.0.zip';
+    return urlBase + 'IEDriverServer_Win32_2.49.0.zip';
   }
 }
 
